7a7a388206466290e68e5aefa544d5c63833996d
canberk-yilmaz <cyilmaz089@gmail.com>
2022-07-20 22:11:49 +0300

    notes general design create

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="notes">
-    <h1>Notes</h1>
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note" />
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">Submit</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card mb-4" v-for="i in 3" :key="i">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nisi
+          delectus, tempora id fugiat ex impedit, esse porro saepe
+          necessitatibus blanditiis earum atque eius magnam facere consectetur
+          aliquam voluptates mollitia.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
