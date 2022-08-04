4fcbb148b036a3f918360f244c42e1798c65d7be
canberk-yilmaz <cyilmaz089@gmail.com>
2022-08-04 09:36:35 +0300

    edit component

---
+++ src/components/Notes/AddEditNote.vue
@@ -0,0 +1,60 @@
+<!-- eslint-disable vue/no-mutating-props -->
+<template>
+  <div class="card p-4 mb-5" :class="`has-background-${bgColor}-dark`">
+    <label v-if="label" class="label has-text-white">{{ label }}</label>
+    <div class="field">
+      <div class="control">
+        <textarea
+          v-model="modelValue"
+          @input="$emit('update:modelValue', modelValue)"
+          class="textarea"
+          :placeholder="placeholder"
+          ref="textAreaRef"
+        />
+      </div>
+    </div>
+
+    <div class="field is-grouped is-grouped-right">
+      <div class="control">
+        <slot name="buttons" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+  bgColor: {
+    type: String,
+    default: "success",
+  },
+  placeholder: {
+    type: String,
+    default: "Type something...",
+  },
+  label: {
+    type: String,
+    default: "Label Text",
+  },
+});
+
+// eslint-disable-next-line no-unused-vars
+const emits = defineEmits(["update:modelValue"]);
+
+const textAreaRef = ref(null);
+
+const focusTextArea = () => {
+  textAreaRef.value.focus();
+};
+
+defineExpose({
+  focusTextArea,
+});
+</script>
