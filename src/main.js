85aafac8f3d9edba263faec3f5ed274ab6057a21
canberk-yilmaz <cyilmaz089@gmail.com>
2022-08-19 16:45:30 +0300

    change for using router in pinia

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from "vue";
+import { createApp, markRaw } from "vue";
 import { createPinia } from "pinia";
 import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
 
@@ -8,5 +8,9 @@ import router from "@/router";
 import "./assets/main.css";
 
 const pinia = createPinia();
+
+pinia.use(({ store }) => {
+  store.router = markRaw(router);
+});
 
 createApp(App).use(autoAnimatePlugin).use(pinia).use(router).mount("#app");
