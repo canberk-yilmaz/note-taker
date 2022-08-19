462ecd1840e7c1d25713baaa527b95b767c1788e
canberk-yilmaz <cyilmaz089@gmail.com>
2022-08-19 16:46:51 +0300

    getnotes move to store and trigger with init func

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,16 @@
 //imports
 import { onMounted } from "vue";
 import NavBar from "@/components/Layouts/NavBar.vue";
-import { useStoreNotes } from "@/stores/storeNotes";
+import { useStoreAuth } from "@/stores/storeAuth";
 
 //store
-const storeNotes = useStoreNotes();
+
+const storeAuth = useStoreAuth();
+
+//mounted
 
 onMounted(() => {
-  storeNotes.getNotes();
+  storeAuth.init();
 });
 </script>
 
